a86f0ab1237c488a82b89714a05c78a7ae1f01e9
Muhammad Rifqi Alimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-04-27 10:30:46 +0700

    commit 1

---
+++ function.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+  alert("ini video youtube");
+});
+
+audio.addEventListener("click", function audio() {
+  alert("ini Audio wrecking");
+});
+
+gambar.addEventListener("click", function gambar() {
+  alert("ini gambar konoha");
+});
